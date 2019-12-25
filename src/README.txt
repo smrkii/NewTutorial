a87969831735fc736594a11252128edb24f289f3
smrkii <zs4346@student.uni-lj.si>
2019-12-25 22:51:45 +0100

    Initial commit

---
+++ src/README.txt
@@ -0,0 +1,8 @@
+* Potegni lwjgl: https://osdn.net/projects/sfnet_java-game-lib/downloads/Official%20Releases/LWJGL%202.9.3/lwjgl-2.9.3.zip/
+V podanem posnetku potegne en drug lwjgl zip.
+* Nastavi okolje preko tega videa --> https://www.youtube.com/watch?v=Jdkq-aSFEA0
+* V intelij klikni File --> Project Structure --> Dependencies --> klikneš na + in dodaš lwjgl.jar, lwjgl_util.jar ter slick-util.jar
+* V direktorij od jave(jst mam jdk-12.0.2) katero uporabljaš v intelij-u, ponavad se nahaja v C:\Program Files\Java\jdk-12.0.2
+v file bin dodej vse ddl fajle kateri se nahajajo v zipu od lwjgl-ja katerega si podegnil. Ti ddl-ji se nahajajo v direktoriju native,
+izbrat morš pa tiste ddl-je odvisno od operacijskega sistema. Če maš windows pol koperaš ddl-je iz windows datoteke v javo kot je
+opisano zgori.
---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,49 @@
+package engineTester;
+
+import entities.Camera;
+import entities.Entity;
+import models.RawModel;
+import models.TexturedModel;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+import renderGame.DisplayManager;
+import renderGame.Loader;
+import renderGame.OBJLoader;
+import renderGame.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+    public static void main(String[] args) {
+
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+
+
+        RawModel model = OBJLoader.loadObjModel("C:\\Users\\smrki\\Documents\\rg\\NewTutorial\\res\\avto.obj",loader);
+
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("C:\\Users\\smrki\\Documents\\rg\\NewTutorial\\res\\avto_textura.png")));
+
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-20),0,0,0,1);
+
+        Camera camera = new Camera();
+
+        while(!Display.isCloseRequested()){
+            entity.increaseRotation(0, 1, 0);
+            camera.move();
+            renderer.prepare();
+            shader.start();
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+
+    }
+}
